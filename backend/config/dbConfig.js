68af6adeab84efafaa6b37994ef7646274339947
chathuwa-whiz <navodchathushka@gmail.com>
2024-12-16 10:19:58 +0530

    refactor database connection to use environment variable for MongoDB URI

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -2,13 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://siriwardhanarumesh:nevermindsDev123@cluster0.vrcvz.mongodb.net/neverMinds?retryWrites=true&w=majority&appName=Cluster0",
-      {
-        useNewUrlParser: true, // Not required in Mongoose 6+, included for backward compatibility
-        useUnifiedTopology: true, // Not required in Mongoose 6+, included for backward compatibility
-      }
-    );
+    const conn = await mongoose.connect(process.env.MONDO_DB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
