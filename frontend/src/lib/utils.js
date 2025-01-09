b319346586f0004315e676dff6810d30e4bd7cd2
YasithJY <it22136374@my.sliit.lk>
2025-01-09 12:25:30 +0530

    Fix Util.jserror fixed

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,12 +1,6 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs) {
-  return twMerge(clsx(...inputs));
-}
-
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
 
 // Utility function to merge and conditionally apply class names
 export const cn = (...classes) => {
