1ff4fa1950e918808796de5607ec69d2e4f85c10
YasithJY <it22136374@my.sliit.lk>
2025-01-15 20:14:43 +0530

    Style Services and ContactTeam components
     Fixed responsive for all screen sizes.

--- frontend/src/components/ContactTeam.jsx
+++ frontend/src/components/ContactTeam.jsx
@@ -3,30 +3,32 @@ import teampic from '../assets/knight.png';
 
 const ContactTeam = () => {
   return (
-    <div className="grid grid-cols-12 md:flex-row items-center md:items-start py-8 pr-20 bg-white">
+    <div className="grid grid-cols-1 md:grid-cols-12 items-center py-8 md:pl-0 px-6 md:pr-20 bg-white">
       {/* Left Section - Image */}
-      <div className=" h-1/2 md:h-full md:col-span-4 flex items-end px-0">
+      <div className="w-full md:col-span-4 flex justify-center md:justify-start px-0 mb-6 md:mb-0">
         <img
           src={teampic}
           alt="Friendly Knight"
-          className="h-[606px] w-auto] object-contain hover:scale-105 transition-transform duration-300"
+          className="h-60 md:h-[606px] w-auto object-contain hover:scale-105 transition-transform duration-300"
         />
       </div>
 
       {/* Right Section - Content */}
-      <div className="w-full md:col-span-8 mt-6 md:mt-0 md:pl-8 text-center">
-        <h1 className="text-[80px] font-regular mb-4 hover:text-[#f9c226] transition-colors duration-300">Contact Our Friendly Team</h1>
-        <div className="grid grid-cols-4 md:grid-cols-4 gap-4 mb-4">
+      <div className="w-full md:col-span-8 text-center md:text-left">
+        <h1 className="text-[36px] md:text-[55px] lg:text-[80px] font-regular text-center mb-4">
+          Contact Our Friendly Team
+        </h1>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mb-6">
           {[...Array(4)].map((_, i) => (
             <div
               key={i}
-              className="h-80 bg-[#f9c226] rounded-3xl hover:shadow-lg hover:bg-[#f9a826] transition-all duration-300"
+              className="h-40 md:h-80 bg-[#f9c226] rounded-3xl hover:shadow-lg hover:bg-[#f9a826] transition-all duration-300"
             ></div>
           ))}
         </div>
-        <p className="text-gray-700 text-[30px] hover:text-gray-900 transition-colors duration-300 text-center">
-          We're here to help! Whether you have questions, need assistance, or want<br />
-          to share feedback, our team is always ready to provide the support you<br />
+        <p className="text-gray-700 text-[18px] md:text-[24px] lg:text-[30px] text-center hover:text-gray-900 transition-colors duration-300">
+          We're here to help! Whether you have questions, need assistance, or want
+          to share feedback, our team is always ready to provide the support you
           need. Reach out to us today!
         </p>
       </div>
