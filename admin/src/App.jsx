f6096738633cb5d802136585522d59b7d8dbcebc
IT21388934 <it21388934@my.sliit.lk>
2025-01-17 11:49:38 +0530

    feat(admin): add admin project 	new file:   admin/.gitignore 	new file:   admin/README.md 	new file:   admin/eslint.config.js 	new file:   admin/index.html 	new file:   admin/package-lock.json 	new file:   admin/package.json 	new file:   admin/postcss.config.js 	new file:   admin/public/vite.svg 	new file:   admin/src/App.css 	new file:   admin/src/App.jsx 	new file:   admin/src/assets/react.svg 	new file:   admin/src/index.css 	new file:   admin/src/main.jsx 	new file:   admin/tailwind.config.js 	new file:   admin/vite.config.js

---
+++ admin/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <div className="text-3xl bg-black text-green-400 p-6">Lest's develop admin pannel</div>
+    </>
+  )
+}
+
+export default App
