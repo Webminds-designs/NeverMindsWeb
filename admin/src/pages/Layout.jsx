30b9677de1ad0a5ae22a338dc73dd9cf19b392e6
chathuwa-whiz <navodchathushka@gmail.com>
2025-05-08 12:40:26 +0530

    Add Protected routes for admin

--- admin/src/pages/Layout.jsx
+++ admin/src/pages/Layout.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import DashboardNavigater from '../components/DashboardNavigater'
 import { Outlet } from 'react-router-dom'
+import ProtectedRoute from './ProtectedRoute'
 
 export default function Layout() {
   return (
-    <div>
+    <ProtectedRoute>
         <DashboardNavigater />
         <div>
             <Outlet />
         </div>
-    </div>
+    </ProtectedRoute>
   )
 }
